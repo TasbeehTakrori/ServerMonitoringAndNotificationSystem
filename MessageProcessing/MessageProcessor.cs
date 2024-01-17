8936bb973797bba76e3185e328fa5f86892a57fb
Tasbeeh Takrori <tasbeh.takrore@gmail.com>
2024-01-17 16:20:05 +0200

    Create MessageProcessor

---
+++ MessageProcessing/MessageProcessor.cs
@@ -0,0 +1,26 @@
+﻿using MessageProcessing.MessageHandling;
+using MessageProcessing.Repository;
+using RabbitMQClientLibrary;
+
+namespace MessageProcessing
+{
+    internal class MessageProcessor : IMessageProcessor
+    {
+        private readonly IMessageQueueConsumer<ServerStatistics> _consumer;
+        private readonly IMessageHandler<ServerStatistics> _messageHandler;
+
+        public MessageProcessor(
+            IMessageQueueConsumer<ServerStatistics> consumer,
+            IMessageHandler<ServerStatistics> messageHandler)
+        {
+            _consumer = consumer;
+            _messageHandler = messageHandler;
+        }
+
+        public void Run()
+        {
+            _consumer.StartConsumingMessages("ServerStatistics.*", _messageHandler.HandleMessage);
+
+        }
+    }
+}
